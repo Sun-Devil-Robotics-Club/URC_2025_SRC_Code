fdbeff006a4749d42963548b9f1fab11ef4d1fe4
pranav142 <pknadimp@gmail.com>
2023-09-16 14:06:20 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SDRC_URC2025
+Source Code for the Rover Competing in University Rover Challenge 2025 
---
+++ sdrc_bringup/launch/sdrc_gazebo.launch.py
@@ -0,0 +1,37 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_path
+
+def generate_launch_description():
+    urdf_path = os.path.join(get_package_share_path("sdrc_description"), "urdf", "my_robot.urdf.xacro")
+    gazebo_ros_launch_path = os.path.join(get_package_share_path("gazebo_ros"), "launch", "gazebo.launch.py")
+    rviz_config_path = os.path.join(get_package_share_path("sdrc_bringup"), "rviz", "urdf_config.rviz")
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{'robot_description': Command(["xacro", " ", urdf_path])}]
+    )
+
+    gazebo_ros_node = Node(
+        package="gazebo_ros", 
+        executable="spawn_entity.py",
+        arguments=["-topic", "robot_description", "-entity", "my_robot"]
+    )
+
+    rviz2_node = Node(
+        package="rviz2", 
+        executable="rviz2",
+        arguments=['-d', rviz_config_path]
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(gazebo_ros_launch_path)),
+        gazebo_ros_node,
+        rviz2_node
+    ])
