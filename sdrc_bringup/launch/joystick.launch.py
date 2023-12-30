faa4133c8fa833513520074b332d6b1f8e8c525e
pranav142 <pknadimp@gmail.com>
2023-12-30 10:30:08 -0700

    fix(refactor)

--- sdrc_bringup/launch/joystick.launch.py
+++ sdrc_bringup/launch/joystick.launch.py
@@ -5,30 +5,30 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch import LaunchDescription
 
-def generate_launch_description(): 
-    use_sim_time = LaunchConfiguration('use_sim_time')
 
-    joy_params = os.path.join(get_package_share_path('sdrc_bringup'), 'config', 'joystick.yaml')
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration("use_sim_time")
 
-    joy_node = Node(
-        package="joy", 
-        executable="joy_node", 
-        parameters=[joy_params]
+    joy_params = os.path.join(
+        get_package_share_path("sdrc_bringup"), "config", "joystick.yaml"
     )
 
-    teleop_node = Node(
-            package='teleop_twist_joy',
-            executable='teleop_node',
-            name='teleop_node',
-            parameters=[joy_params, {'use_sim_time': use_sim_time}],
-            remappings=[('/cmd_vel','/diff_cont/cmd_vel_unstamped')]
-         )
+    joy_node = Node(package="joy", executable="joy_node", parameters=[joy_params])
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='true',
-            description='Use sim time if true'),
-        joy_node,
-        teleop_node,
-    ])
+    teleop_node = Node(
+        package="teleop_twist_joy",
+        executable="teleop_node",
+        name="teleop_node",
+        parameters=[joy_params, {"use_sim_time": use_sim_time}],
+        remappings=[("/cmd_vel", "/diff_cont/cmd_vel_unstamped")],
+    )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "use_sim_time", default_value="true", description="Use sim time if true"
+            ),
+            joy_node,
+            teleop_node,
+        ]
+    )
